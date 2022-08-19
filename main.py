6497dfb08b50eb9c8212d1943b1129c6b6e9cd20
qu1z5 <Yariksaij@gmail.com>
2022-08-19 19:45:31 +0300

    Update main.py
    соединил две функции в одну
    изменил двойные кавычки на одинарные

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import logging
 import os
-
+from pprint import pprint
 import requests
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
+
 
 
 def process_vacancy_hh(language, period, area):
@@ -32,11 +33,16 @@ def process_vacancy_hh(language, period, area):
             vacancies_found = vacancis['found']
             pages = vacancis['pages']
             for vacancy in vacancis['items']:
-                predicted_salary = predict_rub_salary_for_hh(vacancy.get('salary'))
-                if predicted_salary:
-                    total_salary += predicted_salary
-                    vacancies_processed += 1
-                    average_salary = int(total_salary/vacancies_processed)
+                salary = vacancy.get('salary')
+                if salary:
+                    currency = vacancy.get('salary').get('currency')
+                    salary_from = salary.get('from')
+                    salary_to = salary.get('to')
+                    predicted_salary = predict_rub_salary(currency, salary_from, salary_to)
+                    if predicted_salary:
+                        total_salary += predicted_salary
+                        vacancies_processed += 1
+                        average_salary = int(total_salary/vacancies_processed)
     vacancy_params = {
         'language': language,
         'vacancies_found': vacancies_found,
@@ -44,17 +50,6 @@ def process_vacancy_hh(language, period, area):
         'average_salary': average_salary
     }
     return vacancy_params
-
-
-def predict_rub_salary_for_hh(salary):
-    if salary.get('currency') == 'RUR':
-        if salary['from'] and salary['to']:
-            return (salary['from'] + salary['to'])/2
-        elif salary['from']:
-            return salary['from']*1.2
-        elif salary['to']:
-            return salary['to']*0.8
-
 
 def process_vacancy_sj(language, sj_token, town):
     vacancies_found = 0
@@ -72,13 +67,16 @@ def process_vacancy_sj(language, sj_token, town):
             'town': town,
             'keyword': f'Программист {language}'
         }
-        url = "https://api.superjob.ru/2.0/vacancies/"
+        url = 'https://api.superjob.ru/2.0/vacancies/'
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()
         if response.json()['objects']:
             vacancies_found = response.json()['total']
             for vacancy in response.json()['objects']:
-                predicted_salary = predict_rub_salary_for_superjob(vacancy)
+                currency = vacancy['currency']
+                salary_from = vacancy['payment_from']
+                salary_to = vacancy['payment_to']
+                predicted_salary = predict_rub_salary(currency, salary_to, salary_from)
                 if predicted_salary:
                     total_salary += predicted_salary
                     vacancies_processed += 1
@@ -97,14 +95,14 @@ def process_vacancy_sj(language, sj_token, town):
     return vacancy_params
 
 
-def predict_rub_salary_for_superjob(vacancy):
-    if vacancy.get('currency') == 'rub':
-        if vacancy['payment_from'] and vacancy['payment_to']:
-            return vacancy['payment_from'] + vacancy['payment_to']/2
-        elif vacancy['payment_from']:
-            return vacancy['payment_from']*1.2
-        elif vacancy['payment_from']:
-            return vacancy['payment_from']*0.8
+def predict_rub_salary(currency, salary_to, salary_from ):
+    if currency == 'rub' or currency == 'RUR':
+        if salary_to and salary_from:
+            return (salary_to + salary_from)/2
+        elif salary_from:
+            return salary_from*1.2
+        elif salary_to:
+            return salary_to*0.8
 
 
 def make_table(languages_information, title):
